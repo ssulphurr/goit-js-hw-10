5a308282845be92e09af6dd3807e66c7fda83b9a
ssulphurr <ssulphurr@gmail.com>
2023-03-21 23:48:44 +0200

    updates index.js

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import './css/styles.css';
+import debounce from 'lodash.debounce';
+
+const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  inputEl: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
+};
+
+refs.inputEl.addEventListener('input', debounce(onType, DEBOUNCE_DELAY));
+
+function fetchCountries(name) {
+  fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages `
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
+
+function onType(evt) {
+  fetchCountries(evt.target.value)
+    .then(countries => renderCountriesList(countries))
+    .catch(error => console.log(error));
+}
+
+function renderCountriesList(countries) {
+  const markup = countries
+    .map(country => {
+      `<li>
+            <p>${country.name.official}</p>
+    </li>`;
+    })
+    .join('');
+  refs.countryList.innerHTML = markup;
+}

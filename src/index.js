24b690c7e99736f5d1ac4b407515a4c041eb2909
ssulphurr <ssulphurr@gmail.com>
2023-03-22 13:03:07 +0200

    adds task without styling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 
@@ -23,18 +24,61 @@ function fetchCountries(name) {
 }
 
 function onType(evt) {
-  fetchCountries(evt.target.value)
-    .then(countries => renderCountriesList(countries))
-    .catch(error => console.log(error));
+  const inputValue = evt.target.value.trim();
+  if (inputValue == '') {
+    refs.countryList.innerHTML = '';
+    refs.countryInfo.innerHTML = '';
+    return;
+  }
+  fetchCountries(inputValue)
+    .then(countries => {
+      if (countries.length > 10) {
+        Notiflix.Notify.failure(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        return;
+      }
+      if (countries.length === 1) {
+        renderOneCounty(countries);
+        return;
+      }
+      renderCountriesList(countries);
+    })
+    .catch(error => {
+      console.log(error);
+      Notiflix.Notify.warning('Oops, there is no country with that name');
+    });
 }
 
 function renderCountriesList(countries) {
+  refs.countryInfo.innerHTML = '';
+
   const markup = countries
     .map(country => {
       return `<li>
-            <p>${country.name.official}</p>
+      <img width="30" src="${country.flags.svg}" alt="${country.name.official}">
+      <p>${country.name.official}</p>
     </li>`;
     })
     .join('');
   refs.countryList.innerHTML = markup;
+
+  refs.countryList.style.listStyleType = 'none';
+  refs.countryList.style.paddingLeft = '10px';
+}
+
+function renderOneCounty(countries) {
+  refs.countryList.innerHTML = '';
+
+  const markup = countries.map(country => {
+    return `<img width="30" src="${country.flags.svg}" alt="${
+      country.name.official
+    }">
+      <p>${country.name.official}</p>
+      <p>${country.capital}</p>    
+      <p>${country.population}</p>
+      <p>${Object.values(country.languages).join(', ')}</p>`;
+  });
+
+  refs.countryInfo.innerHTML = markup;
 }
